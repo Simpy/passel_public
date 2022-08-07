cd71e76e696e0a2f1e8cfa23ecc0903935987d6d
Simpy <110746606+Simpy@users.noreply.github.com>
2022-08-06 20:07:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -212,4 +212,4 @@ async def on_guild_channel_pins_update(channel, last_pin):
 #client.run('')
 
 # TODO If using GitHub diff deployment on HeroKu comment out the above line with '#' and remove '#' from the line below to uncomment it. 
-client.run(os.environ.get('MTAwNTU5MDA3OTA4OTE1MjA2MA.GnhV2U.cG1czJXJH6KWa5SWKdIZhh2iLj155lF0cGlDD8'))
+client.run(os.environ.get('MTAwNTYyNzA2MjAyOTE0NDA5NA.G3-fN_.x_awCj5Ng2qxtizazIklNv8zdNlSuJ3h1Vzm9g'))
